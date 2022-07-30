f35f4aa9262810d900b417a2fc7024813aca1314
boris.s <boris.sidorov.28@gmail.com>
2022-07-31 02:52:52 +0300

    Task1

---
+++ sem1/Program.cs
@@ -0,0 +1,16 @@
+﻿//тип_данных имя_переменной = значение
+//тип_данных имя_переменной
+
+int num1 = -3;
+int num2 = 9;
+
+if (num2 == num1 * num1)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+
+//консоль, выведи переменную "rezult"
